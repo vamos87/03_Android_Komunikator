be23b2b741b375dd0ef506e1bbbad7af18788b7a
vamos87 <adrian.lawrentjew@gmail.com>
2019-02-15 19:46:11 +0100

    final commit

---
+++ app/src/main/java/com/example/komunikator/CreateMessageActivity.java
@@ -0,0 +1,31 @@
+package com.example.komunikator;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+//        Intent intent = new Intent(this,ReceiveMessageActivity.class);
+//        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE,messageText);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,messageText);
+        intent.putExtra(Intent.EXTRA_SUBJECT,"temat wiadomosci");
+//        startActivity(intent);
+        String chooserTitle = getString(R.string.chooser);
+        Intent chosenIntent = Intent.createChooser(intent,chooserTitle);
+        startActivity(chosenIntent);
+    }
+}
